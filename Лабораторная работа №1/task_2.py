0f3b44d1c92e17de0f9cf93053425b2c70049b09
energy-ves <slithering.crush@gmail.com>
2024-10-22 21:18:38 +0300

    Update task_2.py

--- Лабораторная работа №1/task_2.py
+++ Лабораторная работа №1/task_2.py
@@ -6,7 +6,6 @@ count_of_chars = 25
 b_one_char = 4
 disk_size_in_b = 1024 * 1024 * disk_size_in_mb
 total_b_in_book = count_of_pages * count_of_lines * count_of_chars * b_one_char
-count_of_book = disk_size_in_b // total_b_in_book
-count_of_book = int(count_of_book)
+count_of_book = disk_size_in_b // total_b_in_book #class float
+count_of_book = int(count_of_book) #class int
 print("Количество книг, помещающихся на дискету:", count_of_book)
-
