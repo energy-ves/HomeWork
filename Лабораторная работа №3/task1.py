b6ee7d6abede35897cd035ba5cccecc439d4ded5
energy-ves <slithering.crush@gmail.com>
2024-11-03 22:31:19 +0300

    Add files via upload

---
+++ Лабораторная работа №3/task1.py
@@ -0,0 +1,12 @@
+# TODO Напишите функцию для поиска индекса товара
+def find_index(list_of_items, item):
+    for index, current_item in enumerate(list_of_items):
+        if current_item == item:
+            return index
+items_list = ['яблоко', 'банан', 'апельсин', 'груша', 'киви', 'банан']
+for find_item in ['банан', 'груша', 'персик']:
+    index_item = find_index(items_list, find_item)  # TODO Вызовите функцию, что получить индекс товара
+    if index_item is not None:
+        print(f"Первое вхождение товара '{find_item}' имеет индекс {index_item}.")
+    else:
+        print(f"Товар '{find_item}' не найден в списке.")
